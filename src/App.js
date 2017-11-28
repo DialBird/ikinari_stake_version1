4bbb4c32be1e709ea0963c865453bda2a65dffa6
keisuke <rememberthatk@icloud.com>
2017-11-28 10:57:39 +0900

    App.jsにRouterを組み込んで、ログインの仕組みを完成させる

--- src/App.js
+++ src/App.js
@@ -4,54 +4,32 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React from 'react';
+import { createRootNavigator } from './Router';
+import { isSignedIn } from './auth';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+console.diableYellowBox = true;
 
-export default class App extends Component<{}> {
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { signedIn: false, checkedSignIn: false };
+  }
+
+  componentWillMount() {
+    isSignedIn()
+      .then(res => {
+        this.setState({ signedIn: res, checkedSignIn: true});
+      })
+      .catch(err => console.log('An error occurrd: ' + err));
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+    const { checkedSignIn, signedIn } = this.state;
+
+    if (!checkedSignIn) return null;
+
+    const Router = createRootNavigator(signedIn);
+    return <Router />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
