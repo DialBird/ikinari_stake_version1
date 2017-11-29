94d3af91ad88d8e1ab5fdd804dd2aeadf3c4d741
keisuke <rememberthatk@icloud.com>
2017-11-29 23:30:25 +0900

    その他ビューにログアウト機能を追加する

--- src/components/screens/OthersScreen.js
+++ src/components/screens/OthersScreen.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { Alert, View } from 'react-native';
+import { List, ListItem } from 'react-native-elements';
+import { onSignOut } from '../../auth';
 
-export default () => {
-  return (
-    <View>
-    </View>
-  );
-};
+class OthersScreen extends React.Component {
+  onPress() {
+    Alert.alert(
+      'サインアウト',
+      'よろしいですか？',
+      [
+        {
+          text: 'はい',
+          onPress: () => {
+            onSignOut()
+              .then(()=>this.props.navigation.navigate('SignedOut'))
+              .catch(err => alert('ログアウトに失敗しました: ' + err));
+          }
+        },
+        {text: 'いいえ', style: 'cancel'}
+      ],
+      { cancelable: false }
+    );
+  }
+
+  render() {
+    const list = [
+      { title: 'ログアウト', icon: 'do-not-disturb', color: '#f00' }
+    ];
+    return (
+      <View>
+        <List>
+          {
+            list.map((item, i) => (
+              <ListItem
+                key={i}
+                title={item.title}
+                leftIcon={{name: item.icon, color: item.color}}
+                onPress={this.onPress.bind(this)}
+              />
+            ))
+          }
+        </List>
+      </View>
+    );
+  }
+}
+export default OthersScreen;
