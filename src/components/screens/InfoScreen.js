b7ed12db7aa36922ba076998faa58c75a305c60c
keisuke <rememberthatk@icloud.com>
2017-11-29 09:51:43 +0900

    お知らせ画面を完成させる

--- src/components/screens/InfoScreen.js
+++ src/components/screens/InfoScreen.js
@@ -3,11 +3,65 @@ import {
   StyleSheet,
   Text,
   View,
+  ListView,
+  TouchableOpacity
 } from 'react-native';
+import axios from 'axios';
 
-export default () => {
-  return (
-    <View>
-    </View>
-  );
-};
+class InfoScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = { data: [] };
+  }
+
+  componentWillMount() {
+    axios.get('http://lala.work/api/v1/info_articles')
+      .then((res)=>this.setState({ data: res.data }))
+      .catch((e)=>alert(`axios get error: ${e.message}`));
+  }
+
+  renderRow(info) {
+    return (
+      <TouchableOpacity style={styles.row}>
+        <Text style={styles.rowText}>{info.issue}</Text>
+        <Text style={styles.rowText}>{info.title}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  renderSeparator(sectionId, rowId) {
+    return <View key={rowId} style={styles.separator} />;
+  }
+
+  render() {
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1!==r2});
+    return (
+      <View>
+        <ListView
+          enableEmptySections
+          dataSource={ds.cloneWithRows(this.state.data)}
+          renderRow={this.renderRow}
+          renderSeparator={this.renderSeparator}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  row: {
+    backgroundColor: '#fff',
+    height: 100
+  },
+  rowText: {
+    fontSize: 16,
+    padding: 10
+  },
+  separator: {
+    flex: 1,
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: '#8E8E8E',
+  }
+});
+
+export default InfoScreen;
