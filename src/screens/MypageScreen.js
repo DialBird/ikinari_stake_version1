ed34d2aa100d8eaeec72ec502b8a3f2f0926cb02
keisuke <rememberthatk@icloud.com>
2017-12-14 09:43:47 +0900

    マイページを修正して、綺麗にする

--- src/screens/MypageScreen.js
+++ src/screens/MypageScreen.js
@@ -13,32 +13,31 @@ class MypageScreen extends React.Component {
     this.user = props.navigation.state.params.user;
   }
 
-  splitCode() {
-    let codeArr = [];
-    for (let i=0;i<16;i+=4) {
-      codeArr.push(this.user.code.slice(i, i+4));
-    }
-    return codeArr.join(' ');
+  separatedCode() {
+    return this.user.code.split(/(....)/).filter(Boolean).join(' ');
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={{flex: 1}}>
+        <View style={styles.imageContainer}>
           <Image
             style={styles.image}
-            source={require('../images/cat_with_bird.jpg')}
+            source={require('../images/card.png')}
             resizeMode={'cover'}
           />
         </View>
-        <Barcode
-          value={this.splitCode(this.splitCode)}
-          format="CODE128"
-          height={50}
-          width={1.5}
-        />
+        <View style={styles.barcodeContainer}>
+          <Barcode
+            value={this.separatedCode()}
+            format="CODE128"
+            height={50}
+            width={1.5}
+          />
+          <Text style={{fontSize:20, fontWeight: 'bold'}}>{this.separatedCode()}</Text>
+        </View>
         <View style={{flex: 1, alignItems: 'center'}}>
-          <Text style={styles.title}>現在のあなたの鳥マイレージは</Text>
+          <Text style={styles.text}>現在のあなたの鳥マイレージは</Text>
           <Text style={styles.text}>
             <Text style={styles.point}>{this.user.point}</Text>
             ポイントです
@@ -54,15 +53,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff'
   },
-  image: {
-    flex: 1,
-    height: undefined,
-    width: undefined
+  imageContainer: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  title: {
-    fontSize: 24,
-    textAlign: 'center',
-    paddingVertical: 10
+  image: {
+    width: 256,
+    height: 256
+  },
+  barcodeContainer: {
+    flex: 1,
+    alignItems: 'center'
   },
   text: {
     fontSize: 16
